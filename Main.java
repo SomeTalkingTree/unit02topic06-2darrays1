268f8e24bd80abf0cd93628ebc86c360d246bdd6
Danny Nguyen <25dnguyeuyen20@gamil.com>
2023-02-23 12:31:18 -0600

    something

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.lang.reflect.Array;
 
 import javax.swing.plaf.synth.SynthScrollPaneUI;
  
-public class Main {
+public class Main { /* 
   //fix this again
   public static Array[][] copySquare(int[][] str){
     Array[][] copy= new Array[str.length][str.length];
@@ -16,7 +16,7 @@ public class Main {
     return copy;
   }
  //code needs to be changed and changed
-  public static int[][] copyRectangle(int[][] str){
+  public static Array[][] copyRectangle(int[][] str){
     Array[][] copy= new Array[str.length][str[0].length];
     
     for(int row=0;row<str.length;row++){
@@ -27,7 +27,7 @@ public class Main {
     }
     return copy;
   }
-
+*/
 //Good?
   public static String copyRagged(int[][] str){
     String copy = "";
@@ -42,18 +42,54 @@ public class Main {
   
 
 
-//fix this
-  public static Array[][] printTranspose(double[][] str){
-    Array[][] copy= new Array[str.length][str[0].length];
+//Done
+  public static void printTranspose(double[][] str){
+    double[][] copy= new double[str.length][str[0].length];
     
     for(int tall=0;tall<str[0].length;tall++){
       for (int value=0;value<str.length;value++) {
-        //copy[tall][value] = str[tall][value];
+        copy[value][tall] = str[value][tall];
+        
       }
     }
-    return copy;
+    String text ="";
+    for(int tall=0;tall<str[0].length;tall++){
+      for (int value=0;value<str.length;value++) {
+        text+=copy[value][tall]+" ";
+        
+      }
+      System.out.println(text);
+      text = "";
+    }
+    
   }
 
+
+
+//nice
+//needs fixing
+  public static void withWeightedAverage(double[][] str,double[] weight) {
+  
+  double answer = 0;
+  double total = 0;
+  int num =0;
+  for(int loop=0;loop<weight.length;loop++){
+    answer+=weight[loop];
+  }
+  if(answer==1){  
+  for(double[] row : str){
+    for (double value : row) {
+      total +=value;  
+    }
+    total+= str[num][str[0].length-1];
+    answer = (total/(str[0].length+1));
+    System.out.println(answer);
+    total=0;answer=0;num++;
+    
+    }
+    
+    
+  }
 
   public static void main(String[] args) {
     int[][] list1 = new int[][] {
@@ -66,7 +102,7 @@ public class Main {
       {30,60,76},
       {55,96,85}
     };
-
+    double[] weight = new double[]{0.25,0.25,0.5};
     double[][] doublelist = new double[][] {
     {99, 85, 98},
     {98, 57, 79},
@@ -79,9 +115,11 @@ public class Main {
     {77, 70.5, 66.5},
     {89, 89.5, 81}
     };
-    int num = doublelist.length;
+    int num=doublelist.length;
     int num2 = doublelist[0].length;
-    System.out.println(printTranspose(doublelist));
+    System.out.println(num+" "+num2);
+    //printTranspose(doublelist);
+    withWeightedAverage(doublelist,weight);
     //System.out.println(list[1][2]);
     
     //System.out.println(copySquare(list));
